012d31c14a6e89e6c972f518239363840815f473
admbaskara001 <adhambaskara2@gmail.com>
2023-09-08 15:10:24 +0700

    simpan kode java ke github

---
+++ MyFirstJavaGithub02.java
@@ -0,0 +1,10 @@
+package Daspro_praktikum2_02;
+
+public class MyFirstJavaGithub02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from github");
+        
+    }
+    
+}
